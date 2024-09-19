ec6e287e4a44d47542836e61a32b0d02fedbba7b
Balfegoru <duccoducco@gmail.com>
2024-09-20 00:07:54 +0300

    1

--- Python/PythonDevelop/Seminars/Seminar 3/Dopolnenie/dop1.py
+++ Python/PythonDevelop/Seminars/Seminar 3/Dopolnenie/dop1.py
@@ -5,3 +5,22 @@
 # Пример:
 
 # для k = 9 список будет выглядеть так: [-21 ,13, -8, 5, −3, 2, −1, 1, 0, 1, 1, 2, 3, 5, 8, 13, 21] [Негафибоначчи]​
+
+k = 11
+list1 = [i for i in range(k)]
+print(list1)
+for i in range(len(list1)):
+    if i >=2 :
+        list1[i] = list1[i-1] + list1[i - 2]
+
+
+list2 = [ i for i in list1[::-1]]
+for i in range(len(list2)):
+    if i % 2 == 0:
+        list2[i] = list2[i] * -1
+
+print(list2)
+
+resultList = list2[:-1] + list1[:]
+
+print(resultList)
--- Python/PythonDevelop/Seminars/Seminar 3/readme.txt
+++
@@ -1,2 +0,0 @@
-https://gb.ru/lessons/451028
-link for lesson 2
