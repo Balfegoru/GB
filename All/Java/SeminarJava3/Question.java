686da08cc7c14024ca223215f169bf739d43e153
Balfegoru <duccoducco@gmail.com>
2024-09-29 19:41:53 +0300

    1

--- All/Java/SeminarJava3/Question.java
+++ All/Java/SeminarJava3/Question.java
@@ -6,6 +6,30 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 
+class TwoDimentionalArrayList<T> extends ArrayList<ArrayList<T>> {
+    public void addToInnerArray(int index, T element) {
+        while (index >= this.size()) {
+            this.add(new ArrayList<T>());
+        }
+        this.get(index).add(element);
+    }
+
+    public void addToInnerArray(int index, int index2, T element) {
+        while (index >= this.size()) {
+            this.add(new ArrayList<T>());
+        }
+
+        ArrayList<T> inner = this.get(index);
+        while (index2 >= inner.size()) {
+            inner.add(null);
+        }
+
+        inner.set(index2, element);
+    }
+}
+
+
+
 class FilterNegative {
     public static int[] filterNegative(int[] a) {
         ArrayList<Integer> result = new ArrayList<>();
