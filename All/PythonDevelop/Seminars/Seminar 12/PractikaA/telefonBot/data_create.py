3df71e524d7045b6df9fc24e89106aa7a734c5b3
Balfegoru <duccoducco@gmail.com>
2024-09-30 20:54:04 +0300

    1

---
+++ All/PythonDevelop/Seminars/Seminar 12/PractikaA/telefonBot/data_create.py
@@ -0,0 +1,19 @@
+def name_data():
+    name = input('Введите ваше имя: ')
+    print('Nice name!')
+    return name
+
+def surname_data():
+    surname = input('Введите вашу фамилию: ')
+    print('Nice surname!')
+    return surname
+
+def phone_data():
+    phone = input('Введите ваш телефон: ')
+    print('Nice phone!')
+    return phone
+
+def address_data():
+    address = input('Введите ваш адресс: ')
+    print('Nice address!')
+    return address
